bd6f2e9c18cba2934d475b51d1da81b1f927b546
Egor Vorozhtsov <yegor.vorozhtsov@sharp-dev.net>
2025-11-15 21:10:10 +0600

    starred videos & star button

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -48,22 +48,22 @@ export class ApiService {
 
   getMe(): Observable<any> {
     return this.http.get(this.getUrl('/me'), { headers: this.getHeaders() })
-      .pipe(catchError(err => this.handleError(err)));
+    .pipe(catchError(err => this.handleError(err)));
   }
 
   searchChannels(query: string): Observable<any> {
     return this.http.post(this.getUrl('/channels/search'), { q: query }, { headers: this.getHeaders() })
-      .pipe(catchError(err => this.handleError(err)));
+    .pipe(catchError(err => this.handleError(err)));
   }
 
   searchVideos(query: string, offset: number = 0, limit: number = 20): Observable<any> {
     return this.http.post(this.getUrl('/search/videos'), { q: query, offset, limit }, { headers: this.getHeaders() })
-      .pipe(catchError(err => this.handleError(err)));
+    .pipe(catchError(err => this.handleError(err)));
   }
 
   getChannel(channelId: string): Observable<any> {
     return this.http.get(this.getUrl(`/channels/${channelId}`), { headers: this.getHeaders() })
-      .pipe(catchError(err => this.handleError(err)));
+    .pipe(catchError(err => this.handleError(err)));
   }
 
   getChannelVideos(channelId: string, page: number = 1, pageSize?: number): Observable<any> {
@@ -72,12 +72,12 @@ export class ApiService {
       params = params.set('page_size', pageSize.toString());
     }
     return this.http.get(this.getUrl(`/channels/${channelId}/videos`), { headers: this.getHeaders(), params })
-      .pipe(catchError(err => this.handleError(err)));
+    .pipe(catchError(err => this.handleError(err)));
   }
 
   getChannelPlaylists(channelId: string): Observable<any> {
     return this.http.get(this.getUrl(`/channels/${channelId}/playlists`), { headers: this.getHeaders() })
-      .pipe(catchError(err => this.handleError(err)));
+    .pipe(catchError(err => this.handleError(err)));
   }
 
   getPlaylistVideos(playlistId: string, page: number = 1, pageSize?: number): Observable<any> {
@@ -86,67 +86,94 @@ export class ApiService {
       params = params.set('page_size', pageSize.toString());
     }
     return this.http.get(this.getUrl(`/playlists/${playlistId}/videos`), { headers: this.getHeaders(), params })
-      .pipe(catchError(err => this.handleError(err)));
+    .pipe(catchError(err => this.handleError(err)));
   }
 
   getVideo(videoId: string) {
     return this.http.get<YtDlpVideoInfo>(this.getUrl(`/videos/${videoId}`), { headers: this.getHeaders() })
-      .pipe(catchError(err => this.handleError(err)));
+    .pipe(catchError(err => this.handleError(err)));
   }
 
   getVideoStream(videoId: string, quality?: string): Observable<any> {
     var params = quality ? new HttpParams().set('quality', quality) : new HttpParams();
     return this.http.get(this.getUrl(`/videos/${videoId}/stream`), { headers: this.getHeaders(), params })
-      .pipe(catchError(err => this.handleError(err)));
+    .pipe(catchError(err => this.handleError(err)));
   }
 
   getSubscriptions(): Observable<any> {
     return this.http.get(this.getUrl('/subscriptions'), { headers: this.getHeaders() })
-      .pipe(catchError(err => this.handleError(err)));
+    .pipe(catchError(err => this.handleError(err)));
   }
 
   subscribe(channelId: string): Observable<any> {
     return this.http.post(this.getUrl('/subscriptions'), { yt_channel_id: channelId }, { headers: this.getHeaders() })
-      .pipe(catchError(err => this.handleError(err)));
+    .pipe(catchError(err => this.handleError(err)));
   }
 
   unsubscribe(subscriptionId: number): Observable<any> {
     return this.http.delete(this.getUrl(`/subscriptions/${subscriptionId}`), { headers: this.getHeaders() })
-      .pipe(catchError(err => this.handleError(err)));
+    .pipe(catchError(err => this.handleError(err)));
+  }
+
+  getStarred(): Observable<any> {
+    return this.http.get(this.getUrl('/starred/starred'), { headers: this.getHeaders() })
+    .pipe(catchError(err => this.handleError(err)));
+  }
+
+  addStarred(ytVideoId: string, title: string, thumbnail: string, duration?: number, ytChannelId?: string, channelName?: string): Observable<any> {
+    return this.http.post(this.getUrl('/starred/starred'), {
+      yt_video_id: ytVideoId,
+      title,
+      thumbnail,
+      duration: duration || null,
+      yt_channel_id: ytChannelId || null,
+      channel_name: channelName || null
+    }, { headers: this.getHeaders() })
+    .pipe(catchError(err => this.handleError(err)));
+  }
+
+  removeStarred(ytVideoId: string): Observable<any> {
+    return this.http.delete(this.getUrl(`/starred/starred/${ytVideoId}`), { headers: this.getHeaders() })
+    .pipe(catchError(err => this.handleError(err)));
+  }
+
+  checkStarred(ytVideoId: string): Observable<any> {
+    return this.http.get(this.getUrl(`/starred/starred/${ytVideoId}`), { headers: this.getHeaders() })
+    .pipe(catchError(err => this.handleError(err)));
   }
 
   getSettings(): Observable<any> {
     return this.http.get(this.getUrl('/settings'), { headers: this.getHeaders() })
-      .pipe(catchError(err => this.handleError(err)));
+    .pipe(catchError(err => this.handleError(err)));
   }
 
   updateSetting(key: string, value: any): Observable<any> {
     return this.http.put(this.getUrl(`/settings/${key}`), { value }, { headers: this.getHeaders() })
-      .pipe(catchError(err => this.handleError(err)));
+    .pipe(catchError(err => this.handleError(err)));
   }
 
   getAccounts(): Observable<any> {
     return this.http.get(this.getUrl('/admin/accounts'), { headers: this.getHeaders(true) })
-      .pipe(catchError(err => this.handleError(err)));
+    .pipe(catchError(err => this.handleError(err)));
   }
 
   getAccount(id: string): Observable<any> {
     return this.http.get(this.getUrl(`/admin/accounts/${id}`), { headers: this.getHeaders(true) })
-      .pipe(catchError(err => this.handleError(err)));
+    .pipe(catchError(err => this.handleError(err)));
   }
 
   createAccount(name: string): Observable<any> {
     return this.http.post(this.getUrl('/admin/accounts'), { name }, { headers: this.getHeaders(true) })
-      .pipe(catchError(err => this.handleError(err)));
+    .pipe(catchError(err => this.handleError(err)));
   }
 
   updateAccount(id: string, name: string): Observable<any> {
     return this.http.put(this.getUrl(`/admin/accounts/${id}`), { name }, { headers: this.getHeaders(true) })
-      .pipe(catchError(err => this.handleError(err)));
+    .pipe(catchError(err => this.handleError(err)));
   }
 
   deleteAccount(id: string): Observable<any> {
     return this.http.delete(this.getUrl(`/admin/accounts/${id}`), { headers: this.getHeaders(true) })
-      .pipe(catchError(err => this.handleError(err)));
+    .pipe(catchError(err => this.handleError(err)));
   }
 }
